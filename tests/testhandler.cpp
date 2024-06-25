a8cd7bcfdab90f9a0b65d351c6125679184f3bf1
Ilias Woithe <iwoithe@just42.net>
2024-06-25 13:33:14 +0930

    Add number of tests to test results

--- tests/testhandler.cpp
+++ tests/testhandler.cpp
@@ -24,7 +24,7 @@ void TestHandler::addTest(std::string funcName, TestFunc testFunc)
 void TestHandler::printResults()
 {
     std::cout << std::ctime(&m_testResults.time) << std::endl;
-    std::cout << "Successful " << successPercentage() << "%\n" << std::endl;
+    std::cout << "Successful " << successPercentage() << "% out of " << m_funcNames.size() << " tests\n" << std::endl;
     for (auto& [funcName, errCode] : m_testResults.results) {
         switch (errCode) {
             case ErrCode::SUCCESS:
